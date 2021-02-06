c4787386b8b079757addb9a6c40ef59714f394aa
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-02-07 01:23:28 +0200

    Nix: Add more arguments to ‘default.nix’

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ scales won’t be devided in equal octaves on the piano keyboard, and we have so
 wasteful when we have just about 2 and half octaves in 12tet scale and even less in other micro
 scales.
 
-![Screenshot](./artwork/readme-screenshot.png)
+![Screenshot](artwork/readme-screenshot.png)
 
 ## WARNING!
 
@@ -42,7 +42,7 @@ For now you can:
   because MIDIHasKey just writes events to stdout (in its own format) and you can redirect it
   wherever you want.  
   **TODO** For me: Document the MIDI Player API. For now you can reverse-engeneer it by looking
-  inside [this file](./midiplayer-jack-cpp/src/main.c++) for instance. It also will change in the
+  inside [this file](midiplayer-jack-cpp/src/main.c++) for instance. It also will change in the
   future.
 * [GCC](https://gcc.gnu.org/) __>=6.4.*__ (maybe lower, but must support C++17)  
   To build C++ JACK MIDI Player (it’s optional too if you use your own one).
@@ -54,7 +54,7 @@ For now you can:
 This is the recommended way to run it. It’s also the most simple one.
 
 ``` sh
-nix-shell --pure --run 'midihaskey /dev/input/by-id/usb-xxxx_yyyy-event-kbd | midiplayer-jack-cpp'
+nix-shell --pure --arg with-midiplayer-jack-cpp true --run 'midihaskey /dev/input/by-id/usb-xxxx_yyyy-event-kbd | midiplayer-jack-cpp'
 ```
 
 Where `/dev/input/by-id/usb-xxxx_yyyy-event-kbd` is your keyboard device path
@@ -62,11 +62,29 @@ Where `/dev/input/by-id/usb-xxxx_yyyy-event-kbd` is your keyboard device path
 
 *N.B. `--pure` is optional. It just proves that MIDIHasKey has everything it needs provided by Nix.*
 
-You can enter `nix-shell` in development mode to have `cabal`, `ghci` and source packages:
+#### Tuning `nix-shell`
+
+Just look at [default.nix](default.nix)’s arguments to see available options, it’s pretty flexible.
+
+#### Development mode
+
+You can enter `nix-shell` in development mode to have `ghci` repl with all the dependencies inside
+as well as some other development-related stuff:
 
 ``` sh
-nix-shell --pure --arg dev true
+nix-shell --arg run false --arg dev true
 ```
+
+*N.B. `--arg run false` is optional, it just prevents final executables from building and populating
+your `PATH`. If you’re developing you may not need them.*
+
+##### Development mode for C++ JACK MIDI Player
+
+``` sh
+nix-shell --arg dev true --arg with-midiplayer-jack-cpp true
+```
+
+*N.B. There is no `--arg run false` here because you might want to test it with `midihaskey`.*
 
 ### Using [Stack](https://haskellstack.org)
 
--- default.nix
+++ default.nix
@@ -1,48 +1,34 @@
 let sources = import nix/sources.nix; in
 { pkgs ? import sources.nixpkgs {}
+, run  ? true
 , dev  ? false
+
+, with-midihaskey          ? true
+, with-midiplayer-jack-hs  ? true
+, with-midiplayer-jack-cpp ? false
 }:
 let
-  hpWithUtils = pkgs.haskellPackages.extend (self: super:
-    packageSingleton midihaskey-utils
-  );
+  haskellPackagesWithUtils =
+    pkgs.haskellPackages.extend (self: super: packageSingleton midihaskey-utils);
 
-  package = hp: dir: rec {
+  package = haskellPackages: dir: rec {
     name = baseNameOf (toString dir);
-    pkg  = hp.callCabal2nix name (pkgs.lib.cleanSource dir) {};
+    pkg  = haskellPackages.callCabal2nix name (pkgs.lib.cleanSource dir) {};
   };
 
   packageSingleton = { name, pkg }: { "${name}" = pkg; };
 
-  midihaskey-utils   = package pkgs.haskellPackages ./midihaskey-utils;
-  midihaskey         = package hpWithUtils          ./midihaskey;
-  midiplayer-jack-hs = package hpWithUtils          ./midiplayer-jack-hs;
+  midihaskey-utils   = package pkgs.haskellPackages     ./midihaskey-utils;
+  midihaskey         = package haskellPackagesWithUtils ./midihaskey;
+  midiplayer-jack-hs = package haskellPackagesWithUtils ./midiplayer-jack-hs;
 
-  haskellWithAll = hpWithUtils.extend (self: super:
-    packageSingleton midihaskey-utils // # Fork from "haskellWithUtils" doesn’t help to inherit it
-    packageSingleton midihaskey //
-    packageSingleton midiplayer-jack-hs
+  haskellPacakgesWithAll = haskellPackagesWithUtils.extend (self: super:
+    (if with-midihaskey         then packageSingleton midihaskey         else {}) //
+    (if with-midiplayer-jack-hs then packageSingleton midiplayer-jack-hs else {})
   );
 
   onlyExecutables =
     if dev then (x: x) else pkgs.haskell.lib.justStaticExecutables;
-
-  devDependencies =
-    if dev then [
-      (haskellWithAll.ghcWithPackages (p: [
-        p.midihaskey-utils
-        p.midihaskey
-        p.midiplayer-jack-hs
-
-        p.jack
-      ]))
-
-      pkgs.cabal-install
-
-      pkgs.gnumake
-      pkgs.pkg-config
-      pkgs.jack2
-    ] else [];
 
   midiplayer-jack-cpp = pkgs.stdenv.mkDerivation {
     name = "MIDIHaskKey-midiplayer-jack-cpp";
@@ -63,15 +49,35 @@ let
     meta.license = pkgs.stdenv.lib.licenses.gpl3;
     meta.platforms = pkgs.stdenv.lib.platforms.linux;
   };
+
+  runDependencies =
+    (if with-midihaskey          then [(onlyExecutables midihaskey.pkg)]         else []) ++
+    (if with-midiplayer-jack-hs  then [(onlyExecutables midiplayer-jack-hs.pkg)] else []) ++
+    (if with-midiplayer-jack-cpp then [midiplayer-jack-cpp]                      else []);
+
+  devDependencies =
+    (if with-midihaskey || with-midiplayer-jack-hs then [
+      (haskellPacakgesWithAll.ghcWithPackages (p:
+        [ p.midihaskey-utils ] ++
+        (if with-midihaskey         then [ p.midihaskey ]                else []) ++
+        (if with-midiplayer-jack-hs then [ p.midiplayer-jack-hs p.jack ] else [])
+      ))
+
+      pkgs.cabal-install
+    ] else []) ++
+
+    (if with-midiplayer-jack-cpp then [
+      pkgs.gnumake
+      pkgs.pkg-config
+      pkgs.jack2
+    ] else []);
 in
 pkgs.stdenv.mkDerivation {
   name = "MIDIHasKey";
 
-  buildInputs = [
-    (onlyExecutables midihaskey.pkg)
-    (onlyExecutables midiplayer-jack-hs.pkg)
-    midiplayer-jack-cpp
-  ] ++ devDependencies;
+  buildInputs =
+    (if run then runDependencies else []) ++
+    (if dev then devDependencies else []);
 
   meta.license = pkgs.stdenv.lib.licenses.gpl3;
   meta.platforms = pkgs.stdenv.lib.platforms.linux;
